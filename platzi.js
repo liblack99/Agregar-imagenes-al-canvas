8147b819ca9996dbfd6ff9fe5ed77ffe00d5dc67
Andres fleipe <101063020+liblack99@users.noreply.github.com>
2022-03-11 17:43:11 -0500

    Update platzi.js

--- platzi.js
+++ platzi.js
@@ -1,146 +1,134 @@
-var teclas ={
-UP:38,
-DOWN:40,
-LEFT:37,
-RIGHT:39
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39,
 };
-document.addEventListener("keydown", mover)
+
+document.addEventListener("keydown", mover);
 var vp = document.getElementById("villa_platzi");
 var papel = vp.getContext("2d");
 var xl = 150;
 var yl = 150;
-var cantidad = aleatorio(0, 10)
-
-
-var fondo =
+var cantidad = aleatorio(0, 10);
+var vacas = [];
+var pollos = [];
+for (var v = 0; v < cantidad; v++)
 {
+  vacas.push({ x: aleatorio(0, 10) * 50, y: aleatorio(0, 10) * 50 });
+  pollos.push({ x: aleatorio(0, 10) * 50, y: aleatorio(0, 10) * 50 });
+}
+
+var fondo = {
   url: "tile.png",
-  cargaOK: false
-}
+  cargaOK: false,
+};
 
-var vaca =
-{
+var vaca = {
   url: "vaca.png",
-  cargaOK: false
-}
+  cargaOK: false,
+};
 
-var cerdo =
-{
+var cerdo = {
   url: "cerdo.png",
   cargaOK: false,
-}
+};
 
-var pollo =
-{
+var pollo = {
   url: "pollo.png",
-  cargaOK: false
-}
+  cargaOK: false,
+};
 
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load", cargarFondo);
 
-vaca.imagen = new Image()
+vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVaca);
 
-cerdo.imagen = new Image()
+cerdo.imagen = new Image();
 cerdo.imagen.src = cerdo.url;
 cerdo.imagen.addEventListener("load", cargarCerdo);
 
-pollo.imagen = new Image()
+pollo.imagen = new Image();
 pollo.imagen.src = pollo.url;
 pollo.imagen.addEventListener("load", cargarPollo);
 
-function cargarFondo()
-{
+function cargarFondo() {
   fondo.cargaOK = true;
   dibujar();
 }
-function cargarVaca()
-{
+function cargarVaca() {
   vaca.cargaOK = true;
   dibujar();
-
 }
-function cargarCerdo()
-{
+function cargarCerdo() {
   cerdo.cargaOK = true;
   dibujar();
 }
 
-function cargarPollo()
-{
+function cargarPollo() {
   pollo.cargaOK = true;
   dibujar();
 }
 
-function dibujar()
-{
-  if (fondo.cargaOK == true)
-  {
+function dibujar() {
+  if (fondo.cargaOK == true) {
     papel.drawImage(fondo.imagen, 0, 0);
   }
 
   if (vaca.cargaOK == true)
   {
-    for (var v=0; v < cantidad; v++)
-    {
-      var x= aleatorio(0, 7) * 80;
-      var y= aleatorio(0, 7) * 80;
-    papel.drawImage(vaca.imagen, x, y);
-    }
+    vacas.forEach((item) => {
+      papel.drawImage(vaca.imagen, item.x, item.y);
+    });
   }
-    if (cerdo.cargaOK == true)
-    {
+  if (cerdo.cargaOK == true)
+  {
     papel.drawImage(cerdo.imagen, xl, yl);
-    }
+  }
 
   if (pollo.cargaOK == true)
   {
-    for (var p=0; p < cantidad; p++)
-  {
-    var x= aleatorio(0, 7) * 80;
-    var y= aleatorio(0, 7) * 80;
-    papel.drawImage(pollo.imagen, x, y);
+    pollos.forEach((item) => {
+      papel.drawImage(pollo.imagen, item.x, item.y);
+    });
+    }
   }
-}
-}
 
-function aleatorio(min, maxi)
-{
+
+function aleatorio(min, maxi) {
   var resultado;
   resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
   return resultado;
 }
 
-function mover(evento)
-{
-  var movimiento = 10 ;
-  switch (evento.keyCode)
-  {
+function mover(evento) {
+  var movimiento = 10;
+  switch (evento.keyCode) {
     case teclas.UP:
-    (xl, yl - movimiento);
-    yl = yl - movimiento;
-    dibujar()
-    break;
+      xl, yl - movimiento;
+      yl = yl - movimiento;
+      dibujar();
+      break;
 
     case teclas.DOWN:
-    (xl, yl + movimiento);
-    yl = yl + movimiento;
-    dibujar()
-    break;
+      xl, yl + movimiento;
+      yl = yl + movimiento;
+      dibujar();
+      break;
 
     case teclas.LEFT:
-    (xl - movimiento, yl);
-    xl = xl - movimiento;
-    dibujar()
-    break;
+      xl - movimiento, yl;
+      xl = xl - movimiento;
+      dibujar();
+      break;
 
     case teclas.RIGHT:
-    (xl + movimiento, yl);
-    xl = xl + movimiento;
-    dibujar()
-    break;
+      xl + movimiento, yl;
+      xl = xl + movimiento;
+      dibujar();
+      break;
   }
-  }
+}
